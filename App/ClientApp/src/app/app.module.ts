02f4505fb50c5a53aec9a7ab5991d9704824e6be
esmaeel motaghi <esmaeel.m@live.com>
2022-03-18 09:35:33 +0330

    front and back now seprated

--- App/ClientApp/src/app/app.module.ts
+++ App/ClientApp/src/app/app.module.ts
@@ -7,8 +7,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./front/front.module').then(m => m.FrontModule) },
+  { path: '', redirectTo: 'management', pathMatch: 'full' },
+  //{ path: 'home', loadChildren: () => import('./front/front.module').then(m => m.FrontModule) },
   { path: 'management', loadChildren: () => import('./back/back-wrapper-module').then(m => m.BackWrapperModule) },
 
 ];
